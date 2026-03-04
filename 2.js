d8bf3242622fa329ae47e4b0405d87e812a19ee8
Yoinel <yoinelmartinez04@gmail.com>
2026-03-04 08:03:04 -0500

    Realize el punto 1 y 2 de la guia

---
+++ 2.js
@@ -0,0 +1,2 @@
+let base = prompt("Ingresa la base del rectángulo:"); let altura = prompt("Ingresa la altura del rectángulo:"); let area = base * altura;
+console.log("El área del rectángulo es: " + area);
